b3c8a6a158813e8f8d4cafbb81869393978b0199
toshitaka18 <toshitaka0018@gmail.com>
2019-11-22 20:09:22 +0900

    s3-setting3

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,6 +11,8 @@ set :repo_url,  'git@github.com:yuki-mtmr/freemarket_sample_63f.git'
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
+
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
