57a476d7e47171326a34038d0161aa6789aa8e23
Kazuya-Sakamoto <sskazuya1130@gmail.com>
2019-11-29 16:14:27 +0900

    Item.migrate
    アイテムのカラム共有のため

--- db/migrate/20191120051248_create_items.rb
+++ db/migrate/20191120051248_create_items.rb
@@ -1,7 +1,18 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-
+      t.integer "price", null: false
+      t.string "name", default: "", null: false
+      t.string "image", default: "", null: false
+      t.string "condition", default: "", null: false
+      t.text "description", null: false
+      t.string "item_size", default: ""
+      t.string "region", default: "", null: false
+      t.string "postage", default: "", null: false
+      t.string "shipping_date", default: "", null: false
+      t.string "status" ,        default: "", null: false   
+      t.string "saller_id"
+      t.string "buyer_id", default: ""
       t.timestamps
     end
   end
