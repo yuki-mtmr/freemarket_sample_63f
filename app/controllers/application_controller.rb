37503ff1dd18ba663f7af602e9234eec85e26ba0
toshitaka18 <toshitaka0018@gmail.com>
2019-12-12 15:54:00 +0900

    basic-admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,21 @@
 class ApplicationController < ActionController::Base
-    
-    protect_from_forgery with: :exception
-    skip_before_action :verify_authenticity_token
+  before_action :basic_auth, if: :production?
+  protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token
 
-    def index
+  def index
+  end
+
+  private
+  
+  def production?
+    Rails.env.production?
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
 end
 
